74d605abddedebf4aed3b3023ac5ce993b515a00
EzequielTrejoG <ezegom01@gmail.com>
2024-08-23 21:38:43 -0600

    conexion.php

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+    $conexion = mysqli_connect("127.0.0.1","root","","Regularizacion");
+    //$conexion = mysqli_connect("192.168.16.198","Ezequiel","[R[8sX@h3iC)0eO4","Escolar");
+
+    /*
+    if($conexion)
+    {
+        echo "Se conectó a la base de datos <br><br>";
+    }
+    else
+    {
+        echo "Hubo un error en la conexión ";
+    }
+    */
+?>
